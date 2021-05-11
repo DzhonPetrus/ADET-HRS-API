2d38dfd3c7467951869b67c4946d1cd07fce478c
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-05-11 13:14:08 +0800

    amenity params updated

--- src/routes/amenity.js
+++ src/routes/amenity.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { findAll, findOne, create, update, destroy } = require('../controllers/amenity.controller');
 
 router.get('/', findAll);
-router.get('/:id', findOne);
+router.get('/:amenity_id', findOne);
 router.post('/', create);
-router.put('/:id', update);
+router.put('/:amenity_id', update);
 router.delete('/', destroy);
 
 module.exports = router;
