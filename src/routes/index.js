1145b1ca2effe5e4e155ccde07902d1e474d4853
Princes Millannie Buena <princesbuena03@gmail.com>
2021-05-13 16:03:53 +0800

    housekeeping routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const user = require('./user');
 const amenity = require('./amenity');
+const housekeeping = require('./housekeeping');
 
 module.exports = {
     user,
-    amenity
+    amenity,
+    housekeeping
 };
