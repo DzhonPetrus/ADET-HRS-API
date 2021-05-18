5c8b41ab83c7f036fd8fe2f6e8e0265e54b1d492
Bryon Alindugan <bryonalindugan@gmail.com>
2021-05-18 18:52:41 +0800

    package-api added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const user = require('./user');
 const amenity = require('./amenity');
+const package = require('./package');
 
 module.exports = {
     user,
-    amenity
+    amenity,
+    package
 };
