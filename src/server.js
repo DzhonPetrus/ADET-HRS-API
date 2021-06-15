a9fec2efe95a1517886591e17596ab72b4e5b9a7
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-06-16 02:56:59 +0800

    cors added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const helmet= require('helmet');
 const fs= require('fs');
@@ -29,6 +30,7 @@ const accessLogStream = fs.createWriteStream(
 // API MIDDLEWARES
 app.use(helmet());
 app.use(morgan('combined', { stream: accessLogStream }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
