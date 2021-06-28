8bf34541b1664950df6e2f2b2c7e3ee00dad9e87
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-06-28 22:05:12 +0800

    id -> taxCode

--- src/routes/tax.js
+++ src/routes/tax.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { findAll, findOne, create, update, destroy } = require('../controllers/tax.controller');
 
 router.get('/', findAll);
-router.get('/:id', findOne);
+router.get('/:taxCode', findOne);
 router.post('/', create);
-router.put('/:id', update);
+router.put('/:taxCode', update);
 router.delete('/', destroy);
 
 module.exports = router;
