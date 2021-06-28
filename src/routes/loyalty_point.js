1e7d5461ac0a82a08c184393e70c8f498ce63e28
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-06-28 23:33:09 +0800

    id name fixed

--- src/routes/loyalty_point.js
+++ src/routes/loyalty_point.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { findAll, findOne, create, update, destroy } = require('../controllers/loyalty_point.controller');
 
 router.get('/', findAll);
-router.get('/:id', findOne);
+router.get('/:loyalty_point_id', findOne);
 router.post('/', create);
-router.put('/:id', update);
+router.put('/:loyalty_point_id', update);
 router.delete('/', destroy);
 
 module.exports = router;
